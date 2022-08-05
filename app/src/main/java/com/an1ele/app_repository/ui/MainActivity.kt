39c2d0ff524f2dcec4a150f37902aa4979637d49
Aniele Aquino <anifariasaquino@gmail.com>
2022-08-05 17:10:04 -0300

    adicionando os packages necessarios

--- app/src/main/java/com/an1ele/app_repository/ui/MainActivity.kt
+++ app/src/main/java/com/an1ele/app_repository/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.an1ele.app_repository
+package com.an1ele.app_repository.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.an1ele.app_repository.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
